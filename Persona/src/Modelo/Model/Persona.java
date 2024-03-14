aa01d61be55023a296a14ae82d7ec44ed12aab74
tingwu420 <tingwu420@gmail.com>
2024-03-14 14:17:40 +0100

    first commit

---
+++ Persona/src/Modelo/Model/Persona.java
@@ -0,0 +1,66 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private int DNI;
+
+    /**
+     * 
+     */
+    private void Nombre String;
+
+    /**
+     * 
+     */
+    private void Apellido1 String;
+
+    /**
+     * 
+     */
+    private void Apellido2 String;
+
+    /**
+     * 
+     */
+    private void edad int = 0;
+
+    /**
+     * @param String dni 
+     * @param String nombre 
+     * @param String apellido1 
+     * @param String apellido2 
+     * @param int edad
+     */
+    public void Persona(void String dni, void String nombre, void String apellido1, void String apellido2, void int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param String dni
+     */
+    public void getDni(void String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String setDni() {
+        // TODO implement here
+        return "";
+    }
+
+}
