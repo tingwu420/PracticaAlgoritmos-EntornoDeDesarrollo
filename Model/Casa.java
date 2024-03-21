9d2f8e2432bfcb81482ecdea36d55426575044e1
tingwu420 <tingwu420@gmail.com>
2024-03-21 09:47:58 +0100

    Casa

---
+++ Model/Casa.java
@@ -0,0 +1,97 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Casa {
+
+    /**
+     * Default constructor
+     */
+    public Casa() {
+    }
+
+    /**
+     * 
+     */
+    private String direccion;
+
+    /**
+     * 
+     */
+    private int numHabitaciones;
+
+    /**
+     * 
+     */
+    private int metrosCuadrados;
+
+    /**
+     * 
+     */
+    public void toString;
+
+    /**
+     * @param direccion 
+     * @param numHabitaciones 
+     * @param metrosCuadrados
+     */
+    public void Casa(String direccion, int numHabitaciones, int metrosCuadrados) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getDireccion() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public int getNumHabitaciones() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public int getMetrosCuadrados() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param direccion:String
+     */
+    public void setDireccion(void direccion:String) {
+        // TODO implement here
+    }
+
+    /**
+     * @param numHabitaciones:int
+     */
+    public void setNumHabitaciones(void numHabitaciones:int) {
+        // TODO implement here
+    }
+
+    /**
+     * @param metrosCuadrados:int
+     */
+    public void setMetrosCuadrados(void metrosCuadrados:int) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String toString() {
+        // TODO implement here
+        return "";
+    }
+
+}
---
+++ Model/Persona.java
@@ -0,0 +1,126 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private int DNI;
+
+    /**
+     * 
+     */
+    private String Nombre;
+
+    /**
+     * 
+     */
+    private String Apellido1;
+
+    /**
+     * 
+     */
+    private String Apellido2;
+
+    /**
+     * 
+     */
+    private void edad int = 0;
+
+    /**
+     * @param String dni 
+     * @param String nombre 
+     * @param String apellido1 
+     * @param String apellido2 
+     * @param int edad
+     */
+    public void Persona(void String dni, void String nombre, void String apellido1, void String apellido2, void int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param String dni
+     */
+    public void getDni(void String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String setDni() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public String getApellido1() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param Apellido1
+     */
+    public void setApellido1(String Apellido1) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getApellido2() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param Apellido2
+     */
+    public void setApellido2(String Apellido2) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param edad
+     */
+    public void setEdad(int edad) {
+        // TODO implement here
+    }
+
+}
