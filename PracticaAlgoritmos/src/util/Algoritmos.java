732da9a7d2060881cbb01746e7c09ca362216e1c
tingwu420 <162586562+tingwu420@users.noreply.github.com>
2024-04-20 17:12:35 +0200

    Metodo Fibonacci

--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -11,15 +11,6 @@ public abstract class Algoritmos {
     }
 	
 	
-	// Método para calcular el factorial de un número
-    public static int factorial(int numero) {
-        if (numero == 0) {
-            return 1;
-        } else {
-            return numero * factorial(numero - 1);
-        }
-    }
-    
-    
+
     
 }
