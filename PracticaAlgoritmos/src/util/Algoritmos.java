ca160a82ba7e668ca35c09e71532e2651110e01e
tingwu420 <162586562+tingwu420@users.noreply.github.com>
2024-04-20 17:06:15 +0200

    Metodo Factorial

--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -1,6 +1,7 @@
 package util;
 
 public abstract class Algoritmos {
+	
 	public static int fibonacci(int numero) {
         if (numero <= 1) {
             return numero;
@@ -8,4 +9,17 @@ public abstract class Algoritmos {
             return fibonacci(numero - 1) + fibonacci(numero - 2);
         }
     }
+	
+	
+	// Método para calcular el factorial de un número
+    public static int factorial(int numero) {
+        if (numero == 0) {
+            return 1;
+        } else {
+            return numero * factorial(numero - 1);
+        }
+    }
+    
+    
+    
 }
